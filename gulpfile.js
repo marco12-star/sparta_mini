f97237eb0ab0f4a7ebff05b9cae962fda5d94fa7
marco12-star <62327943+marco12-star@users.noreply.github.com>
2021-07-02 22:07:20 +0300

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -65,9 +65,7 @@ function browserSync(params) {
 function html() {
 	return src(path.src.html)
 		.pipe(fileinclude())
-		.pipe(htmlmin({
-			collapseWhitespace: true
-		}))
+		.pipe(htmlmin())
 		.pipe(webphtml())
 		.pipe(dest(path.build.html))
 		.pipe(browsersync.stream())
