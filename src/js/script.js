85dd1c5784b7a86046c5260dc163203efa7f39a7
marco12-star <62327943+marco12-star@users.noreply.github.com>
2021-07-04 19:46:52 +0300

    Academy start

--- src/js/script.js
+++ src/js/script.js
@@ -22,3 +22,33 @@ window.onscroll = function () {
         header.classList.remove("sticky");
     }
 };
+
+/* Academy Tabs */
+
+$('[name=academyType]').on('change', function () {
+    var p = $(this).prop('checked');
+
+    var i = $('[name=academyType]').index(this);
+
+    $('.academy__form_wrap').removeClass('on');
+    $('.academy__form_wrap').eq(i).addClass('on');
+
+    if ($('.academy__form_wrap').eq(i).hasClass('coach_tab')) {
+        $('#remainSteps').html('8')
+    } else {
+        $('#remainSteps').html('10')
+    }
+});
+
+document.getElementById('loadAva').addEventListener('change', readURL, true);
+
+function readURL() {
+    var file = document.getElementById("loadAva").files[0];
+    var reader = new FileReader();
+    reader.onloadend = function () {
+        document.getElementById('loadAvaLabel').style.background = "url(" + reader.result + ") 100%/cover no-repeat";
+    }
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {}
+}
