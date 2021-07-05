bf641adad47170680c0763980b63bf16bde809e8
marco12-star <62327943+marco12-star@users.noreply.github.com>
2021-07-05 16:10:41 +0300

    ы

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 /* Hamburger Menu */
-var bar = $('#bar')
-var html = $('html')
+var bar = document.getElementById("bar")
+var html = document.querySelector('html')
 
-bar.click(function () {
-    if (html.hasClass('open_menu')) {
-        html.removeClass('open_menu')
+bar.addEventListener('click', function() {
+    if (html.classList.contains('open_menu')) {
+        html.classList.remove('open_menu')
     } else {
-        html.addClass('open_menu')
+        html.classList.add('open_menu')
     }
-});
+})
 
 
 /* Fixed Header */
@@ -22,38 +22,3 @@ window.onscroll = function () {
         header.classList.remove("sticky");
     }
 };
-
-/* Academy Tabs */
-
-$('[name=academyType]').on('change', function () {
-    var p = $(this).prop('checked');
-
-    var i = $('[name=academyType]').index(this);
-
-    $('.academy__form_wrap').removeClass('on');
-    $('.academy__form_wrap').eq(i).addClass('on');
-
-    if ($('.academy__form_wrap').eq(i).hasClass('coach_tab')) {
-        $('#remainSteps').html('8')
-    } else {
-        $('#remainSteps').html('10')
-    }
-});
-
-if ($('.academy__form_wrap').hasClass('coach_tab')) {
-    $('#remainSteps').html('8')
-} else {
-    $('#remainSteps').html('10')
-}
-
-
-function readURL() {
-    var file = document.getElementById("loadAva").files[0];
-    var reader = new FileReader();
-    reader.onloadend = function () {
-        document.getElementById('loadAvaLabel').style.background = "url(" + reader.result + ") 100%/cover no-repeat";
-    }
-    if (file) {
-        reader.readAsDataURL(file);
-    } else {}
-}
